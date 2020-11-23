23740dc9d13a403444059668a6bd1bde9d41a2e9
chaima <bouchibachaima@gmail.com>
2020-11-23 18:10:34 +0200

    Bootstrap JQuery

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 1500 } );
+    $("#carousel-pause").click(function(){
+        $("#mycarousel").carousel('pause');
+    });
+    $("#carousel-play").click(function(){
+        $("#mycarousel").carousel('cycle');
+    });
+});
